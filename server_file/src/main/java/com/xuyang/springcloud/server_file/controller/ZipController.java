bdcc4c6d7fc185fcf65e03b0a522f801ec1a1ec3
xuy <15172399690>
2019-03-28 18:43:14 +0800

    文件接口部门开发提交

--- server_file/src/main/java/com/xuyang/springcloud/server_file/controller/ZipController.java
+++ server_file/src/main/java/com/xuyang/springcloud/server_file/controller/ZipController.java
@@ -1,7 +1,12 @@
 package com.xuyang.springcloud.server_file.controller;
 
+import com.xuyang.springcloud.server_file.model.json.ResultJson;
+import com.xuyang.springcloud.server_file.model.json.XmlJson;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,4 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class ZipController {
+
+    @RequestMapping(value = "zipWar.do", method = RequestMethod.GET)
+    public ResultJson zipWar(@RequestBody XmlJson xmlJson){
+        String filePath = xmlJson.getFilePath();
+        return null;
+    }
 }
