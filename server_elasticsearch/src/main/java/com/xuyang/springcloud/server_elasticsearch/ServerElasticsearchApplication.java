eab66e0ac5688a5cfe285e673f4232103a2a7782
xuy <15172399690>
2019-04-08 13:54:45 +0800

    elasticSearch开始集成

---
+++ server_elasticsearch/src/main/java/com/xuyang/springcloud/server_elasticsearch/ServerElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.xuyang.springcloud.server_elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerElasticsearchApplication.class, args);
+	}
+
+}
---
+++ server_elasticsearch/src/test/java/com/xuyang/springcloud/server_elasticsearch/ServerElasticsearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xuyang.springcloud.server_elasticsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerElasticsearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
