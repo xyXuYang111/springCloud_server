559ec93700715363542fb177ff6108e3cba1b56a
xuy <15172399690>
2019-04-08 10:45:46 +0800

    二维码生成maven依赖

--- server_api/src/main/java/com/xuyang/springcloud/server_api/matrix/MatrixUtil.java
+++ server_api/src/main/java/com/xuyang/springcloud/server_api/matrix/MatrixUtil.java
@@ -8,7 +8,6 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.xuyang.springcloud.server_api.def.MatrixDef;
-import com.xuyang.springcloud.server_api.model.Matrix;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
