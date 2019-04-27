e5bc73f28207542f4fb96b7e87b8397db1b92676
xuy <15172399690>
2019-04-27 15:00:55 +0800

    mongodb入门集成

---
+++ server_mongodb/src/main/java/com/xuyang/springcloud/server_mongodb/ServerMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.xuyang.springcloud.server_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerMongodbApplication.class, args);
+    }
+
+}
